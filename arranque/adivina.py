13f7e6db3f55d2df18ff58a8d5f859eaf1ba58db
Katia-GM <katia.gabriela.gm@gmail.com>
2026-08-19 17:17:52 -0600

    agrega notas de python

--- arranque/adivina.py
+++ arranque/adivina.py
@@ -6,8 +6,39 @@
 #  líneas que crees que va a imprimir. Luego lo corremos.
 # =========================================================
 
-a = 3
-b = 4
+a = 3 #variable: lugar donde se guarda un valor especifico
+#integer int 
+nombre = "Katia"
+edad = 17
+b = "hola soy un string"
+#string str es una cadena de texto
+c= True #1
+d= False #0 
+#booleanos bool datos olo verdaderos o falsos
+
+e= .03 
+#float numeros con punto decimal
+# print() sirve para mostrar información en la consola
+print("Hola mundo")
+# Se pueden combinar variables de distintos tipos usando +. 
+nombre = "Katia"
+saludo = "Hola" + nombre
+print (saludo)
+
+#También se pueden usar f-strings para combinar texto y variables.
+edad = 17
+print(f"Hola {nombre}, tienes {edad} años.")
+
+# input () perimte recibir información escrita por el usuario
+nombre = input("¿Cuál es tu nombre? ")
+print (f"Hola {nombre}, bienvenido a la escudería GEIIA.")
+# if permite tomar decisiones.
+if edad >= 18:
+    print("Eres mayor de edad.")
+else:
+    print("Eres menor de edad.")
+
+
 
 print(a + b)
 print("a + b")

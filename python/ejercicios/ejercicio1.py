9807178cb5bda75dffe394a40fb45b38c7c06ac3
rogeliomc910-sketch <rogeliomc910@gmail.com>
2026-08-20 18:54:17 -0600

    Tarearogerficha

--- python/ejercicios/ejercicio1.py
+++ python/ejercicios/ejercicio1.py
@@ -18,3 +18,34 @@
 #Has vivido 6935 días
 #De esos, has dormido 2312 días completos
 #Este semestre vas a pasar 26 horas y 40 minutos en el camino
+
+
+#Tareita roger
+nombre = input("¿Cuál es tu nombre? ")
+edad = int(input("¿Cuántos años tienes? "))
+horas_dormir = float(input("¿Cuántas horas duermes al día? "))
+minutos_uanl = int(input("¿Cuántos minutos tardas en llegar a la UANL? "))
+
+#dias vividos
+dias_vividos = edad * 365
+
+#horas vividas
+horas_vividas = dias_vividos * 24
+
+#horas dormidas
+horas_dormidas = horas_vividas * horas_dormir
+
+#dias completos dormidos
+dias_dormidos = round(horas_dormidas / 24)
+
+#traslado en un semestre
+minutos_semestre = minutos_uanl * 2 * 5 * 16
+horas_semestre = minutos_semestre // 60
+minutos_restantes = minutos_semestre % 60
+
+#Ficha
+print("=== FICHA DE PILOTO ===")
+print(nombre + ", " + str(edad) + " años")
+print("Has vivido " + str(dias_vividos) + " días")
+print("De esos, has dormido " + str(dias_dormidos) + " días completos")
+print("Este semestre vas a pasar " + str(horas_semestre) + " horas y " + str(minutos_restantes) + " minutos en el camino")

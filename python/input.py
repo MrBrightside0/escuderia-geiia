bc379b979c41e4fd563c1a2421722212b7c9ecfd
draago818 <daosato@gmail.com>
2026-08-20 20:41:01 -0600

    Guardado de otros archivos en rama de rodolfo

---
+++ python/input.py
@@ -0,0 +1,28 @@
+#edad = input("Cuantos años tienes? ") #input vuelve todo lo que metiste en la terminal
+#como string
+
+#print(type(edad))
+
+
+#convertir un dato en otro
+#int() 
+#str()
+#bool()
+#float()
+#print(int(edad) + 1) #convirtiendo str a int
+#print(float(edad) + 1) #conviertiendo str a float
+#print(bool(edad) + 1) #conviertiendo str a booleano 0 1 2 3 4 5
+#texto = "hola"
+#print(int(texto)) hay ciertos cambios que no se pueden hacer   
+#print(int(13.9))   #quita despues del punto antes de convertir el dato    
+# round() = redondea un flotante
+
+nombre = input("Cual es tu nombre? ")
+edad = 19
+estatura = 1.79
+desayuno = True
+
+
+#print(nombre + " tiene " + str(edad) + " años ")
+#texto2 = nombre + " tiene " + str(edad) + " años "
+#print(texto2)
---
+++ python/tipos_de_datos.py
@@ -0,0 +1,26 @@
+#tipo de datos
+
+
+a = 3 #variable : es un lugar en donde se puede guardar valor 
+#integer int
+
+b = "hola soy un string" 
+#string str es una cadena de texto 
+
+c = True # 1
+d = False # 0 
+#Booleanos Bool datos que solo peuden ser verdaderos o falsos
+
+e = .03 #1.1 1 .8 243354435546554 3234.5432323 
+#float float son aquellos numeros que tinene punto decimal
+
+#print(3 + a) #mostrar el valor de la variable o de lo que queramos mostrar 
+
+print(type(e)) #conocer tipo de dato de variables 
+
+#numero positivo = True
+#numero negativo = False
+#strings al no tener caracter asignado 
+variable = 1
+print(bool(variable), bool(variable - 2))
+#python esta escrito en C

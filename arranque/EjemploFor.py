10d087f1386903a5cee10d8d704e273c54b9ac5e
janeth-moreno <abigayiyi@gmail.com>
2026-08-26 17:39:03 -0600

    Ejercicos_For

---
+++ arranque/EjemploFor.py
@@ -0,0 +1,23 @@
+
+#Ejemplo 1 (Listo dentro del bucle)
+for i in range(7):
+    print (i)
+    print("listo")
+ 
+ #Ejemplo 2 (Listo fuera del bucle)
+for i in range(7):
+    print(i)    
+print ("Listo")
+
+#Ejemplo 3 (Listas)
+escuderia= ["Janeth", "Rodolfo", "Rogelio", "Katia", "Joseph"]
+for e in escuderia:
+   print("Hola, yo soy:",e)
+print("Listo!")
+
+#EJERCICIO Imprime los números del 1 al 10
+for i in range (1,11):
+ print(i)
+
+for i in range (1,11):
+    if i % 2 == 0: print(i)

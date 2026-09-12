f51de016107257c4b94fd9b77b3a03f634a7da62
draago818 <daosato@gmail.com>
2026-09-11 18:07:10 -0600

    coso para pasar las tareas a texto y q se guarden, de Rodolfo

--- python/pycache/main.py
+++ python/pycache/main.py
@@ -3,11 +3,19 @@ from tarea import volver_al_menu, menu_principal, eleccion_de_menu
 tareas = []
 opcion = 0
 
+with open ("tarea.txt") as f:
+    for i in f:
+        tareas.append(i.strip())
+
 while opcion != 5:
     opcion = menu_principal()
-    tareas = eleccion_de_menu(opcion,tareas)
-                         
+    tareas = eleccion_de_menu(opcion,tareas)   
+
 if len(tareas) != 0:     
     print("Termina tus tareas no seas flojo")
 else:
     print("Bien hecho, puedes ver anime todo lo q quieras :3")    
+
+with open ("tarea.txt", "w") as f:
+    for i in tareas:
+        f.write(i + "\n")
